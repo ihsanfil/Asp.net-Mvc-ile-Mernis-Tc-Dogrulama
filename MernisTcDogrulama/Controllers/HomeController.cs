a8fe4430e1aef8bc061f9d4e54821deee4e76225
ihsan <42092819+ihsanfil@users.noreply.github.com>
2021-02-09 20:27:07 +0300

    Add project files.

---
+++ MernisTcDogrulama/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MernisTcDogrulama.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public int TcDogrula(long tcNo, int dogumYili , string ad , string soyad)
+        {
+            bool? dogruMu = false;
+            try
+            {
+                using (tcKimlikServis.KPSPublicSoapClient tcServis = new tcKimlikServis.KPSPublicSoapClient())
+                {
+                    dogruMu = tcServis.TCKimlikNoDogrula(tcNo, ad, soyad, dogumYili);
+                }
+            }
+            catch (Exception)
+            {
+                dogruMu = null;
+            }
+            if (dogruMu == true)
+            {
+                return  1; //DOĞRUYSA
+            }
+            if (dogruMu == false)
+            {
+                return 2; //YANLIŞSA
+            }
+            return 0;
+        }
+    }
+}
---
+++ MernisTcDogrulama/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MernisTcDogrulama.MvcApplication" Language="C#" %>
